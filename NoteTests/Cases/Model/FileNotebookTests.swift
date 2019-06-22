a89467164fceafdaebc4a31cf684a8c07f27646c
Brovko Roman <brovko-roman@ya.ru>
2019-06-22 12:12:51 +0300

    my improve task#03

--- NoteTests/Cases/Model/FileNotebookTests.swift
+++ NoteTests/Cases/Model/FileNotebookTests.swift
@@ -45,14 +45,16 @@ class FileNotebookTests: XCTestCase {
         
         XCTAssertEqual(notes.count, 1)
         
-        XCTAssertNotNil(notes[note.uid])
+        let checkedNote = getNote(by: note.uid, from: notes)
+        
+        XCTAssertNotNil(checkedNote)
     }
     
     func testFileNotebook_whenAddNote_noteSavedInNotesWithAllInfo() {
         let note = Note(title: "Title", content: "Text", importance: .normal)
         sut.add(note)
 
-        guard let checkedNote = sut.notes[note.uid] else {
+        guard let checkedNote = getNote(by: note.uid, from: sut.notes) else {
             XCTFail()
             return
         }
@@ -74,7 +76,7 @@ class FileNotebookTests: XCTestCase {
         let note2 = Note(uid: note.uid, title: "New Title", content: "My new text", color: .red, importance: .important, destructDate: Date())
         sut.add(note2)
         
-        guard let checkedNote = sut.notes[note2.uid] else {
+        guard let checkedNote = getNote(by: note2.uid, from: sut.notes) else {
             XCTFail()
             return
         }
@@ -105,7 +107,7 @@ class FileNotebookTests: XCTestCase {
         XCTAssertTrue(notes.isEmpty)
     }
     
-    func testFileNotebook_whenSaveToFileAndRestore_noteRemoveFromNotes() {
+    func testFileNotebook_whenSaveToFileAndLoadFromFile_correctRestoreNotes() {
         let note = Note(title: "Title", content: "Text", importance: .normal)
         sut.add(note)
         
@@ -126,8 +128,70 @@ class FileNotebookTests: XCTestCase {
         
         let notes = sut.notes
         XCTAssertEqual(notes.count, 2)
-        XCTAssertNotNil(notes[note.uid])
-        XCTAssertNotNil(notes[note2.uid])
+        XCTAssertNotNil(getNote(by: note.uid, from: notes))
+        XCTAssertNotNil(getNote(by: note2.uid, from: notes))
+    }
+    
+    func testFileNotebook_whenSaveToFileAndLoadFromFile_equalsRestoredNotes() {
+        let note = Note(title: "Title", content: "Text", importance: .normal)
+        sut.add(note)
+        
+        let note2 = Note(title: "New Title", content: "My new text", color: .red, importance: .important, destructDate: Date())
+        sut.add(note2)
+        
+        sut.saveToFile()
+        
+        sut.restoreFromFile()
+        
+        let notes = sut.notes
+        
+        guard let checkedNote = getNote(by: note.uid, from: notes),
+            let checkedNote2 = getNote(by: note2.uid, from: notes) else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(note.uid, checkedNote.uid)
+        XCTAssertEqual(note.title, checkedNote.title)
+        XCTAssertEqual(note.content, checkedNote.content)
+        XCTAssertEqual(note.importance, checkedNote.importance)
+        XCTAssertEqual(note.color, checkedNote.color)
+        
+        XCTAssertNil(checkedNote.selfDestructDate)
+        
+        guard let checkedDate = checkedNote.selfDestructDate, let date = note.selfDestructDate else {
+            return
+        }
+        
+        XCTAssertEqual(checkedDate, date)
+
+        
+        XCTAssertEqual(note2.uid, checkedNote2.uid)
+        XCTAssertEqual(note2.title, checkedNote2.title)
+        XCTAssertEqual(note2.content, checkedNote2.content)
+        XCTAssertEqual(note2.importance, checkedNote2.importance)
+        XCTAssertEqual(note2.color, checkedNote2.color)
+        
+        XCTAssertNotNil(checkedNote.selfDestructDate)
+        
+        guard let checkedDate2 = checkedNote2.selfDestructDate, let date2 = note2.selfDestructDate else {
+            return
+        }
+        
+        XCTAssertEqual(checkedDate2, date2)
+
     }
 
+
+    private func getNote(by uid: String, from notes:Any) -> Note? {
+        if let notes = notes as? [String: Note] {
+            return notes[uid]
+        }
+        
+        if let notes = notes as? [Note] {
+            return notes.filter { $0.uid == uid }.first
+        }
+        
+        return nil
+    }
 }
